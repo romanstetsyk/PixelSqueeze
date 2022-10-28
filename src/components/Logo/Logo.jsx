df70ac3b77aca5ef9288b20a13dc4921700bd45a
romanstetsyk <25715951+romanstetsyk@users.noreply.github.com>
2022-10-28 17:18:57 +0100

    add manifest.json, change logo link

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -3,7 +3,7 @@ import { LogoLink } from "./Logo.styled";
 export const Logo = () => {
   return (
     <nav>
-      <LogoLink href="/">
+      <LogoLink href={process.env.PUBLIC_URL}>
         <h1>PixelSqueeze</h1>
       </LogoLink>
     </nav>
