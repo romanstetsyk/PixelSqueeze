c157e0a801a0969ee48dfbf66d0103db828f861d
romanstetsyk <25715951+romanstetsyk@users.noreply.github.com>
2022-12-05 01:52:21 +0000

    Card proptypes

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types";
 import { Box } from "components/App/App.styled";
 
 export const Card = ({ title, text }) => {
@@ -7,4 +8,9 @@ export const Card = ({ title, text }) => {
       <p>{text}</p>
     </Box>
   );
+};
+
+Card.propTypes = {
+  title: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
 };
