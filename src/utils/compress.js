048a7c34e5ded3b2e61d2e3ee696c81d3d0d6ed8
romanstetsyk <25715951+romanstetsyk@users.noreply.github.com>
2023-01-03 02:55:04 +0000

    use addEventListener instead of elem.onload

--- src/utils/compress.js
+++ src/utils/compress.js
@@ -1,25 +1,31 @@
 export const compress = (file, quality, type) => {
   return new Promise((ressolve, reject) => {
     let img = new Image();
-    img.onload = () => {
-      const [width, height] = [img.width, img.height];
-      const canvas = document.createElement("canvas");
-      const context = canvas.getContext("2d");
-      canvas.width = width;
-      canvas.height = height;
-      context.drawImage(img, 0, 0, width, height);
-      canvas.toBlob(
-        blob => {
-          const clone = new File([blob], file.name, { type: type });
-          clone.id = file.id;
-          clone.quality = quality;
-          URL.revokeObjectURL(img.src);
-          ressolve(clone);
-        },
-        type,
-        quality / 100 // Quality as a decimal
-      );
-    };
+
+    img.addEventListener(
+      "load",
+      () => {
+        URL.revokeObjectURL(img.src);
+        const [width, height] = [img.width, img.height];
+        const canvas = document.createElement("canvas");
+        const context = canvas.getContext("2d");
+        canvas.width = width;
+        canvas.height = height;
+        context.drawImage(img, 0, 0, width, height);
+        canvas.toBlob(
+          blob => {
+            const clone = new File([blob], file.name, { type: type });
+            clone.id = file.id;
+            clone.quality = quality;
+            ressolve(clone);
+          },
+          type,
+          quality / 100 // Quality as a decimal
+        );
+      },
+      { once: true }
+    );
+
     img.src = URL.createObjectURL(file);
   });
 };
