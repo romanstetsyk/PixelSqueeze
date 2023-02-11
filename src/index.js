3fad3e731022bcd198d2c99a12ad8e7a990008ef
romanstetsyk <25715951+romanstetsyk@users.noreply.github.com>
2023-02-11 03:00:10 +0000

    return strict mode

--- src/index.js
+++ src/index.js
@@ -8,10 +8,10 @@ import { GlobalStyles } from "theme/GlobalStyles";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  
+  <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
       <App />
     </ThemeProvider>
-
+  </React.StrictMode>
 );
